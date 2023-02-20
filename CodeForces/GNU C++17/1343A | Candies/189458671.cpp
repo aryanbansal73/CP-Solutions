1a3a4070a2853203c35cdc64b855360d18687d7d
Aryan Bansal <97313778+aryanbansal73@users.noreply.github.com>
2023-02-20 19:24:47 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1343A | Candies/189458671.cpp
@@ -0,0 +1,193 @@
+/*
+ 
+                          ____,......__
+                     _, -'' _________,..i''--.._
+                 ,.-'' ,.-''             '' ... '`-._
+              ,/' _.-''                        '`-.  '-
+            ,' ,-'                                 ':. `-
+          ,' ,'                                       `. `.
+         / ,'                                           `. \
+       ,' /                       .|                      \ `.
+      /  /                        '\                       \  \
+     / ,'                       ,''`__                      `. \
+    / /                         ':::.|                        \ \
+   / ;                          |+-'i|                         : \
+  ;  ;                         ,',''.|                         :  :
+  ; /                         /,|- --|\                         \ :
++/ /                         ,;'| ;; |\\                         \ \
+; ;                         ,/__,\--i .L`                         : :
+;-;   /|                  ,''|||'    '+-:...                      : :
+ ;._ |..\              _-'    |`      | _''-:`-.           ,''--'' : :
+ |\ / \              ,-       |.\-   ,|/-   |   \         ,' /-    | |
+ ; '`\ ;-         _-'     ,,. '`:`'ii,''   ,_    -.     _:..;:     : :
+;    -' ;+      ,+||,. _,' ,\...--'    ,,-i -`..   '-._,''''        : :
+|     -:  '.. +'||| ||/  /' __,.....'-'  ,'   '-'-.    `  /         | |
+|      \     .  |.  /,/;;,'' |          /         ''-..__'          | |
+|       \   |    `-/' |/|     \        .              \             | |
+|         ''|`._.-'   |'     __-`-.._....:                          | |
+|            '             ,'  ''.---iii  '-_                       | |
+:                        ,'      `\::\      `:'-.                  _; ;
+ :                     ,'     ,._ |\::\      ':_.:=.._______..,--''; ;
+ |                   /'    ,i:,'':+ \::\      | `_ \-._ .__     _. | |
+-:-                /'   _,'' /      :`\:\     .   \ `\ '--.i---'   ; ;
+: :             -;:. ,,-    /        ',i.:___/     \  '-.   '-..  ; ;
+: :       ,-----  i;/'     '         /     '        `    '`-.._,:`;,;
+ \ \       '''--.,-'     ,'        /'                ' .._____,.-+'/
+  : \                   /       ,/'                             / ;
+  :  :                 ||    _,'                               ;  ;
+   \ :                  |    |                                 ; /
+    \ \                  \    \                               / /
+     \ `.                 `    \                            ,' /
+      \  \                 `.  '.                          /  /
+       `. \                  \__i.                        / ,'
+         \ `.                 +''`.                     ,' /
+          `. `.       ,+.._;:-i;.--:  .-.             ,' ,'
+            `. `.    (\i.).-''-=-----+--`\          ,' ,'
+              `. `-.  `+'.---.....|-.|__ +'''':-.,-' ,'
+                `-. `-.../       - ...:=-.|   ,-' ,-'
+                   `-. `--._     : :     _.--' ,-'
+                      `---._''---------''_.---'
+                            ''---------''
+██████╗░██╗░░░░░███████╗░█████╗░░██████╗███████╗░░░░░░░░░░░░░█████╗░░█████╗░░█████╗░███████╗██████╗░████████╗
+██╔══██╗██║░░░░░██╔════╝██╔══██╗██╔════╝██╔════╝░░░░░░░░░░░░██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗╚══██╔══╝
+██████╔╝██║░░░░░█████╗░░███████║╚█████╗░█████╗░░█████╗█████╗███████║██║░░╚═╝██║░░╚═╝█████╗░░██████╔╝░░░██║░░░
+██╔═══╝░██║░░░░░██╔══╝░░██╔══██║░╚═══██╗██╔══╝░░╚════╝╚════╝██╔══██║██║░░██╗██║░░██╗██╔══╝░░██╔═══╝░░░░██║░░░
+██║░░░░░███████╗███████╗██║░░██║██████╔╝███████╗░░░░░░░░░░░░██║░░██║╚█████╔╝╚█████╔╝███████╗██║░░░░░░░░██║░░░
+╚═╝░░░░░╚══════╝╚══════╝╚═╝░░╚═╝╚═════╝░╚══════╝░░░░░░░░░░░░╚═╝░░╚═╝░╚════╝░░╚════╝░╚══════╝╚═╝░░░░░░░░╚═╝░░░
+  #username - a_parzival_b
+*/
+#include <bits/stdc++.h>
+#include<iostream>
+using namespace std;
+#define ll long long int
+int gcd1(int a, int b)
+{
+  return b == 0 ? a : gcd1(b, a % b);
+}
+void primefactors(vector<ll> &v, ll n)
+{
+  while (n % 2 == 0)
+  {
+    v.push_back(2);
+    n /= 2;
+  }
+  for (int i = 3; i <= sqrt(n); i += 2)
+  {
+    while (n % i == 0)
+    {
+      v.push_back(i);
+      n = n / i;
+    }
+  }
+  if (n > 2)
+  {
+    v.push_back(n);
+  }
+}
+int max_map(unordered_map<char, int> &v)
+{
+  int ans = INT_MIN;
+  for (auto it : v)
+  {
+    ans = max(ans, it.second);
+  }
+  return ans;
+}
+int makezero(int n)
+{
+  int ans = INT_MAX;
+  for (int i = 0; i <= 15; i++)
+  {
+    for (int j = 0; j <= 15; j++)
+    {
+      if (int((n + i) * (1 << j)) % 32768 == 0)
+      {
+        ans = min(ans, i + j);
+      }
+    }
+  }
+  return ans;
+}
+bool isprime(ll n){
+  for(int i =2 ; i<=sqrt(n) ; i++){
+    if(n%i == 0){
+      return false;
+    }
+  }
+  return true ;
+}
+int indx(int low ,int high, int x,int ad[] ){
+  while(high - low>1){
+    int mid = (low+high)/2;
+    if(ad[mid]<=x){
+      high = mid ;
+    }
+    else{
+      low = mid  ;
+    }
+ 
+  }
+  if(ad[low]<x){return low; }
+  else {return high ;}
+ 
+}
+bool isvalid(long long x , vector<ll> c , ll n , ll a){
+    set<long long>pq ;
+    int q = n/a  , r= n%a;
+    int k ;
+    sort(c.begin() , c.end());
+    for(int i = 0 ; i<q ; i++ ){
+        for(int j = 0 ; j<a ; j++){
+            k = n-1  - (i*a +j) ; 
+            if(pq.size()<a){
+                pq.insert(c[k]);
+            }
+            else{
+              auto it = pq.begin();
+              auto var =  *it + c[k];
+              pq.erase(it);
+              pq.insert(var);
+            }
+        }
+    }
+    for(int i = 0 ; i <r ; i++){
+      k--;
+      auto it = pq.begin();
+      auto var =  *it + c[k];
+      pq.erase(it);
+      pq.insert(var);
+    }
+    auto mx = pq.end();
+    
+    return *mx <=x ;
+}
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+  int t ;
+  cin>>t;
+  while(t--){
+    int n ;
+    cin>>n;
+    vector<int> v;
+    v.push_back(3);
+    int k =2 ;
+    for(int i = 1 ; i < 30 && v[i-1] + 2*k<=n;i++){
+      k*=2 ;
+      v.push_back(k + v[i-1]);
+    }
+    int x ;
+    for(auto it: v){
+      if(n%it == 0){
+        x = n/it ;
+        cout<<x<<endl;
+        break;
+      }
+    }
+  }
+}
