b791f44844f03079216c233aec6074a51e71ac8e
Aryan Bansal <97313778+aryanbansal73@users.noreply.github.com>
2023-02-20 19:42:55 +0530

    Committing files

---
+++ CodeForces/GNU C++17/507B | Amr and Pins/157786090.cpp
@@ -0,0 +1,34 @@
+/*
+ deepspacetramp
+ aryan
+                         ooOoo
+                       o8888888o
+                       88" . "88
+                       (| -_- |)
+                       O\  =  /O
+                    _/`---'\_
+                  .'  \\|     |//  `.
+                 /  \\|||  :  |||//  \
+                /   ||||| -:- |||||   \
+                |   | \\\  -  //| |   |
+                | \_|  `\`---'/'  |_/ |
+                \  .-\__ `-.-' __/-.  /
+              _`. .'  /--.--\   `. .'_
+           ."" '<  `._\_<|>/__.' _> \"".
+         | | :  `- \`. ;`. _/; .'/ /  .' ; |
+          \  \ `-.   \_\_`. .'/_/  -' _.' /
+===========`-.`_`-._\ \_  /.-'.'_.-'================
+*/
+#include<bits/stdc++.h>
+using namespace std ;
+#define ll  long long
+int main(){
+    ll r , x , y , x1 , y1 ;
+    cin>>r>>x>>y>>x1>>y1 ;
+    ll dx = abs(x-x1) , dy = abs(y-y1);
+    float  dis = (dx*dx) + (dy*dy);
+    dis = sqrt(dis);
+    int dia = 2*r ;
+    ll  ans =  ceil(dis/dia) ;
+    cout<<ans<<endl;
+}
